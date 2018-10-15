6e30bae54676d31891b3bee44f1e7cc07c3014ee
Ilya <mra4niykeks@gmail.com>
2018-10-16 01:13:05 +0300

    remove interfaces

--- src/main/java/money/rbk/test/interfaces/NotificationInterface.java
+++
@@ -1,9 +0,0 @@
-package money.rbk.test.interfaces;
-
-/**
- * Interface to export report information
- */
-public interface NotificationInterface {
-
-    String export();
-}
--- src/main/java/money/rbk/test/interfaces/OuterDataTransactionOuterInterface.java
+++
@@ -1,9 +0,0 @@
-package money.rbk.test.interfaces;
-
-/**
- * Interface to upload csv data from external sources
- */
-public interface OuterDataTransactionOuterInterface {
-
-    String /*todo*/ upload(String data);
-}
