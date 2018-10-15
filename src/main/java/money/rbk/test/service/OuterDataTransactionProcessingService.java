4dfb729ded63b2b728e2aee99b84f1d02dc8b124
Ilya <mra4niykeks@gmail.com>
2018-10-15 16:18:28 +0300

    the xtext has been implemented

--- src/main/java/money/rbk/test/service/OuterDataTransactionProcessingService.java
+++ src/main/java/money/rbk/test/service/OuterDataTransactionProcessingService.java
@@ -1,26 +1,25 @@
 package money.rbk.test.service;
 
-import com.fasterxml.jackson.databind.MappingIterator;
-import com.fasterxml.jackson.dataformat.csv.CsvMapper;
-import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 import com.google.inject.Injector;
 import lombok.extern.slf4j.Slf4j;
 import money.rbk.test.entity.OuterDataEntity;
-import money.rbk.test.model.OuterData;
 import money.rbk.test.repository.OuterDataRepository;
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.eclipse.xtext.resource.XtextResourceSet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.xtext.money.rbkdsl.RbkMoneyDslStandaloneSetup;
 import org.xtext.money.rbkdsl.RbkMoneyDslStandaloneSetupGenerated;
+import org.xtext.money.rbkdsl.rbkMoneyDsl.TotalLine;
+import org.xtext.money.rbkdsl.rbkMoneyDsl.TransactionRecord;
+import org.xtext.money.rbkdsl.rbkMoneyDsl.TransactionsFile;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.List;
 
@@ -37,52 +36,40 @@ public class OuterDataTransactionProcessingService { // todo unification?
     @Autowired
     private OuterDataRepository outerDataRepository;
 
-    public static void main(String[] args) throws IOException {
-        RbkMoneyDslStandaloneSetup.doSetup();
-        RbkMoneyDslStandaloneSetupGenerated sg = new RbkMoneyDslStandaloneSetup();
-        final Injector injector = sg.createInjector();
-
-        String src = "PID;PAMOUNT;PDATA;\n" +
-                "123;94.7;20160101120000;\n" +
-                "124;150.75;20160101120001;\n" +
-                "125;1020.2;20160101120002;\n" +
-                "126;15.5;20160101120003;\n" +
-                "127;120.74;20160101120004;\n" +
-                "TOTAL;5;";
-
-        XMIResourceFactoryImpl resourceFactory = new XMIResourceFactoryImpl();
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(src.getBytes());
-        XtextResourceSet resourceSet = injector.getInstance(XtextResourceSet.class);
-//        resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl());
-        Resource resource = resourceSet.createResource(URI.createURI("1.rbkdsl"));
-        resource.load(inputStream, Collections.EMPTY_MAP);
-
-
-        EObject eObject = resource.getContents().get(0);
-
-        Resource resource1 = resourceSet.createResource(URI.createURI("2.xmi"));
-        resource1.getContents().addAll(resource.getContents());
-
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        resource1.save(byteArrayOutputStream, Collections.EMPTY_MAP);
-
-
-        System.out.println(new String(byteArrayOutputStream.toByteArray()));
-
-    }
-
     public void processOuterInformation(String outerDataContent) {
-        List<OuterData> objectsFromCsv = getObjectsFromCsv(outerDataContent);
-        storeData(objectsFromCsv);
+        List<TransactionsFile> objectsFromCsv = getObjectsFromCsv(outerDataContent);
+        if (objectsFromCsv.isEmpty()) {
+            log.error("Nothing is read");
+            return;
+        }
+        final TransactionsFile transactionsFile = objectsFromCsv.get(0); // should be only one file, means only one total line among transaction records
+        final TotalLine totalLine = transactionsFile.getTotalLine();
+        if (totalLine == null) {
+            log.error("Wrong format! No total section");
+            return;
+        }
+        final BigDecimal total = totalLine.getTotal();
+        if (total.intValue() != transactionsFile.getTransaction().size()) {
+            log.error("Wrong transactions count. Total is: %d actual transaction list size is: %d", total.intValue(), transactionsFile.getTransaction().size());
+            return;
+        }
+        storeData(transactionsFile);
     }
 
-    private void storeData(List<OuterData> outerData) { // todo think: new record will create even if it already is
-//        outerDataRepository.saveAll(outerData);
-        for (OuterData outerDatum : outerData) {
-            outerDataRepository.save(OuterDataEntity.createFromPlainData(outerDatum.getId(), outerDatum.getAmount(),
-                    outerDatum.getData(), NEW)
+    private void storeData(TransactionsFile transactionsFile) { // todo think: new record will create even if it already is
+        for (TransactionRecord record : transactionsFile.getTransaction()) {
+            outerDataRepository.save(OuterDataEntity.createFromPlainData(record.getPid().longValue(), record.getPamount(),
+                    record.getPdata().toString(), NEW)
             );
         }
+    }
+
+    private String getResourceAsXML(XtextResourceSet resourceSet, Resource resource) throws IOException {
+        Resource resource1 = resourceSet.createResource(URI.createURI("name.xmi"));
+        resource1.getContents().addAll(resource.getContents());
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        resource1.save(byteArrayOutputStream, Collections.EMPTY_MAP);
+        return new String(byteArrayOutputStream.toByteArray());
     }
 
     /**
@@ -90,23 +77,26 @@ public class OuterDataTransactionProcessingService { // todo unification?
      *
      * @param outerDataContent csv data from outer source
      */
-    private List<OuterData> getObjectsFromCsv(String outerDataContent) {
 
+    @SuppressWarnings("unchecked")
+    private List<TransactionsFile> getObjectsFromCsv(String outerDataContent) {
 
+        RbkMoneyDslStandaloneSetup.doSetup(); // init model
+        RbkMoneyDslStandaloneSetupGenerated sg = new RbkMoneyDslStandaloneSetup();
+        final Injector injector = sg.createInjector(); // get injector to get environment members
+
+//        XMIResourceFactoryImpl resourceFactory = new XMIResourceFactoryImpl();
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(outerDataContent.getBytes());
+        XtextResourceSet resourceSet = injector.getInstance(XtextResourceSet.class);
+
+        Resource resource = resourceSet.createResource(URI.createURI("name.rbkdsl"));
         try {
-            CsvSchema bootstrapSchema = CsvSchema.builder()
-                    .setColumnSeparator(';')
-                    .setStrictHeaders(true)
-                    .setUseHeader(true)
-                    .build();
-            CsvMapper mapper = new CsvMapper();
-            MappingIterator<OuterData> readValues =
-                    mapper.readerFor(OuterData.class).with(bootstrapSchema).readValues(outerDataContent);
-            return readValues.readAll();
-        } catch (Exception e) {
-            log.error("Error occurred while loading object list", e);
-            return Collections.emptyList();
+            resource.load(inputStream, Collections.EMPTY_MAP);
+        } catch (IOException e) {
+            log.error("Error while reading from resource");
+            return Collections.EMPTY_LIST;
         }
+        return (List<TransactionsFile>) (EList) resource.getContents(); // i am 100% sure in this cast
     }
 
     public List<OuterDataEntity> findOuterDataEntitiesWithStatus(OuterDataEntity.Status status) {
